e4e1e29f06abc5afdfede2d442cae1f94eabab24
Malcome Fein <mfein90@gmail.com>
2018-11-05 00:17:36 -0500

    new methods

--- lib/animal.rb
+++ lib/animal.rb
@@ -1,5 +1,6 @@
 require 'nokogiri'
 require 'open-uri'
+require 'pry'
 
 class Animal
   attr_accessor :name, :kingdom, :phylum, :klass, :order, :family, :genus, :species
@@ -22,6 +23,41 @@ class Animal
     trs[6].search('a').text
   end
 
+  def self.getPhylum(doc)
+    table = doc.search('.biota')
+    trs = table.search('tr')
+    trs[7].search('a').text
+    # binding.pry
+  end
+
+  def self.getKlass(doc)
+    table = doc.search('.biota')
+    trs = table.search('tr')
+    trs[9].search('a').text
+    # binding.pry
+  end
+
+  def self.getOrder(doc)
+    table = doc.search('.biota')
+    trs = table.search('tr')
+    trs[10].search('a').text
+    # binding.pry
+  end
+
+  def self.getFamily(doc)
+    table = doc.search('.biota')
+    trs = table.search('tr')
+    trs[11].search('a').text
+    # binding.pry
+  end
+
+  def self.getGenus(doc)
+    table = doc.search('.biota')
+    trs = table.search('tr')
+    trs[12].search('a').text
+    # binding.pry
+  end
+
   # Constructor -
   def self.new_from_wikipedia(url)
     animal = Animal.new
@@ -31,13 +67,14 @@ class Animal
 
     animal.name = doc.search("h1#firstHeading").text
     animal.kingdom = self.getKingdom(doc)
-    animal.phylum = doc.search("span.phylum").text
-    animal.klass = doc.search("span.klass").text
-    animal.order = doc.search("span.order").text
-    animal.family = doc.search("span.family").text
-    animal.genus = doc.search("span.genus").text
+    animal.phylum = self.getPhylum(doc)
+    animal.klass = self.getKlass(doc)
+    animal.order = self.getOrder(doc)
+    animal.family = self.getFamily(doc)
+    animal.genus = self.getGenus(doc)
 
     animal
+    # binding.pry
   end
 
   def self.new_from_google_search(keyword)
